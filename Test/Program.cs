6820c1d15b663321165800d27561dffb797f71a8
Omarhio <big-dz@hotmail.fr>
2025-06-18 16:38:40 +0200

    Ajoutez des fichiers projet.

---
+++ Test/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Exercice 1
+
+/*int nombre;
+
+Console.WriteLine("Saisissez un nombre :");
+nombre = Convert.ToInt32(Console.ReadLine());
+
+if (nombre > 10)
+{
+    Console.WriteLine("supérieur");
+}
+else
+{
+    Console.WriteLine("Inférieur");
+};*/
+
+
+//Exercice 2
+
+/*int nombre;
+Console.WriteLine("Saisissez un nombre :");
+nombre = Convert.ToInt32(Console.ReadLine());
+
+if (nombre > 0) {
+    Console.WriteLine("Positif");
+        }
+else if (nombre < 0){
+    Console.WriteLine("negatif");
+        }
+else {
+    Console.WriteLine("Nul");
+        };*/
