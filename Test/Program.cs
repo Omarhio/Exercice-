562be914aeb79907b9743a346ebe8c15d0921305
Omarhio <big-dz@hotmail.fr>
2025-06-19 00:08:21 +0200

    Ajout des exercices 3 et 4

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿/*Exercice 1
 
-
-//Exercice 1
-
-/*int nombre;
-
+int nombre;
 Console.WriteLine("Saisissez un nombre :");
 nombre = Convert.ToInt32(Console.ReadLine());
 
@@ -17,19 +13,52 @@ else
     Console.WriteLine("Inférieur");
 };*/
 
+/*Exercice 2
 
-//Exercice 2
-
-/*int nombre;
+int nombre;
 Console.WriteLine("Saisissez un nombre :");
 nombre = Convert.ToInt32(Console.ReadLine());
 
-if (nombre > 0) {
+if (nombre > 0) 
+{
     Console.WriteLine("Positif");
-        }
-else if (nombre < 0){
+}
+else if (nombre < 0)
+{
     Console.WriteLine("negatif");
-        }
-else {
+}
+else 
+{
     Console.WriteLine("Nul");
-        };*/
+};*/
+
+/*Exercice 3
+
+int age;
+
+Console.WriteLine("Saisissez votre age :");
+age = Convert.ToInt32(Console.ReadLine());
+
+if (age > 18)
+{
+    Console.WriteLine("Majeur");
+}
+else
+{ 
+    Console.WriteLine("Mineur");
+};*/
+
+/*Exercice 4
+
+Console.WriteLine("Entrez une chaîne de caractères :");
+string saisie = Console.ReadLine();
+
+if (string.IsNullOrEmpty(saisie))
+{
+    Console.WriteLine("La chaîne est vide.");
+}
+else
+{
+    Console.WriteLine("La chaîne n'est pas vide.");
+}
+*/
